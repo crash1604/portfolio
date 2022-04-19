50730cbfe930584591a9601c2d4bfec8fa85843e
crash1604 <cs.cucse@gmail.com>
2022-04-18 18:50:48 -0700

    minor changes to skills and frameworks

--- src/components/portfolio/frameworks/Frameworks.jsx
+++ src/components/portfolio/frameworks/Frameworks.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../skills/skills.scss';
 
 export default function Frameworks() {
-    const frameworks = ['Django (Python)','Node JS npm (JavaScript)','React JS (JavaScript)','Django Rest Framework '];
+    const frameworks = ['Django (Python)','Node JS npm (JavaScript)','React JS (JavaScript)','Django Rest Framework','StageXL'];
     return <div className='skills'>
      {
         frameworks.map(skill => <p><span>$> </span>{skill}</p>)
--- src/components/portfolio/skills/Skills.jsx
+++ src/components/portfolio/skills/Skills.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './skills.scss';
 
 export default function Skills() {
-    const skillset= ['Python','JavaScript','C & C++','AI and Advanced Algorithms']
+    const skillset= ['Python','JavaScript','C & C++','AI and Advanced Algorithms','Dart']
   return <div className='skills'>
       {
         skillset.map(skill => <p><span>$></span>{skill}</p>)
